a3d9b2e658edd90449bf57713ca969d8ab396d27
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2024-03-19 23:18:19 +0530

    Modified Batch

--- autorun.bat
+++ autorun.bat
@@ -1,10 +1,29 @@
 @echo off
+echo Hello From Team,Kinderneutron .......
+echo.                                     
 
-REM Run docker-compose in detached mode
-docker-compose up -d
+echo Created By Team, Kinderneutron
 
-REM Open a new Command Prompt window and run the first Docker image
-start cmd /k  "docker exec -it kinderneutron_container sh" & title kinderneutron_image
+echo.    
 
-REM Open another new Command Prompt window and run the second Docker image
+echo Setting Up Docker containers...
+timeout /t 3 /nobreak > nul
+REM Run docker-compose in detached mode without displaying output
+echo Running Docker containers...
+docker-compose up -d > nul
+
+REM Display custom message in the Command Prompt
+
+
+REM Wait for 5 seconds (adjust the number as needed)
+echo Executing kinderneutron_image and psql Docker containers...
+timeout /t 5 /nobreak > nul
+
+REM Open a new Command Prompt window and run the first Docker image without displaying output
+start cmd /k "docker exec -it kinderneutron_container sh" & title kinderneutron_image
+
+REM Wait for 3 seconds (adjust the number as needed)
+
+
+REM Open another new Command Prompt window and run the second Docker image without displaying output
 start cmd /k "docker exec -it psql-db sh" & title psql-db
